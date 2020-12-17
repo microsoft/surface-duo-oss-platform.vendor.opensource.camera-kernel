ae7f8af7370ceb876cddcb1b717efbe691f5f81e
Minghao Xue <mingxue@codeaurora.org>
2020-12-17 17:26:33 +0800

    init: define early init functions for camera subsystem
    Here we define init functions for camera subsystem.
    
    Change-Id: I2f32fb685ed9dca56306e980365d847c18a9961e
    Signed-off-by: Minghao Xue <mingxue@codeaurora.org>

--- drivers/cam_sensor_module/cam_csiphy/cam_csiphy_dev.c
+++ drivers/cam_sensor_module/cam_csiphy/cam_csiphy_dev.c
@@ -254,7 +254,7 @@ static void __exit cam_csiphy_exit_module(void)
 	platform_driver_unregister(&csiphy_driver);
 }
 
-module_init(cam_csiphy_init_module);
+early_module_init(cam_csiphy_init_module, EARLY_SUBSYS_3, EARLY_INIT_LEVEL2);
 module_exit(cam_csiphy_exit_module);
 MODULE_DESCRIPTION("CAM CSIPHY driver");
 MODULE_LICENSE("GPL v2");
